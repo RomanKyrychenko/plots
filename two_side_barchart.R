6e0a540cff56e683c49e4f7af989279e72c919ff
RomanKyrychenko <kirichenko17roman@gmail.com>
2017-01-05 17:30:47 +0200

    Initial commit

---
+++ two_side_barchart.R
@@ -0,0 +1,65 @@
+#Код для створення двостороннього барчарту
+
+library(dplyr)
+library(ggplot2)
+library(grid)
+library(gridExtra)
+DATA <- data_frame(
+  state = c("Макоекономічне середовище", "Інновації", "Вища освіта та професійна підготовка", 
+            "Технологічна готовність", "Інституції", "Розмір ринку", "Ефективність ринку товарів", 
+            "Інфраструктура", "Рівень розвитку бізнесу", "Охорона здоров'я та початкова освіта", 
+            "Рівень розвитку фінансового ринку", "Ефективність ринку праці"),
+  "Позитивна динаміка" = c(6,2,1,1,1,0.001,0.0001,0.00001,0.000001,0.0000001,0.00000001,0),
+  "Негативна динаміка" = c(0,0,0,0,0,2,2,6,7,9,9,17)
+)
+
+g.mid<-ggplot(DATA,aes(x=1,y=reorder(state, `Позитивна динаміка`)))+geom_text(aes(label=state))+
+  ggtitle("")+
+  ylab(NULL)+
+  theme(axis.title=element_blank(),
+        panel.grid=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        panel.background=element_blank(),
+        axis.text.x=element_text(color=NA),
+        axis.ticks.x=element_line(color=NA),
+        plot.margin = unit(c(1,-1,1,-1), "mm"))
+
+g1 <- ggplot(data = DATA, aes(x = reorder(state, `Позитивна динаміка`), y = `Негативна динаміка`)) +
+  geom_bar(stat = "identity",fill="#fdd49e") + ggtitle("Негативна динаміка") +
+  scale_y_continuous(limits=c(0,20))+
+  ylab("") +
+  xlab("") +
+  theme(axis.title.x = element_blank(), 
+        axis.title.y = element_blank(), 
+        axis.text.y = element_blank(), 
+        axis.ticks.y = element_blank(), 
+        plot.margin = unit(c(1,-1,1,0), "mm")) +
+  scale_y_reverse() + coord_flip() +
+  theme(axis.line = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank()) 
+
+g2 <- ggplot(data = DATA, aes(x = reorder(state, `Позитивна динаміка`), y = `Позитивна динаміка`)) +xlab(NULL)+
+  geom_bar(stat = "identity",fill="#d9f0a3") + ggtitle("Позитивна динаміка") +
+  scale_y_continuous(limits=c(0,20))+
+  ylab("") +
+  xlab("") +
+  theme(axis.title.x = element_blank(), axis.title.y = element_blank(), 
+        axis.text.y = element_blank(), axis.ticks.y = element_blank(),
+        plot.margin = unit(c(1,0,1,-1), "mm")) +
+  coord_flip()+
+  theme(axis.line = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank()) 
+
+
+gg1 <- ggplot_gtable(ggplot_build(g1))
+gg2 <- ggplot_gtable(ggplot_build(g2))
+gg.mid <- ggplot_gtable(ggplot_build(g.mid))
+
+grid.arrange(gg1,gg.mid,gg2,ncol=3,widths=c(3/9,3/9,3/9))
